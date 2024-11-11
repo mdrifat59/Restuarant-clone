e0c39fb75382f9193cfad0c2f732e40cfc0d995a
mdrifat59 <mdrifatulislam59@gmail.com>
2024-11-12 01:35:02 +0600

    Testimonials section is done

---
+++ src/component/Testimonials/index.jsx
@@ -0,0 +1,160 @@
+import React, { forwardRef } from 'react';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import man from '/public/img/man.png'
+import clon from '/public/img/clon.png'
+import { LeafIcons } from '../../svg/Leaf';
+import video from '/public/img/video.png'
+
+const TestimonialSlider = forwardRef((props, ref) => {
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 1600 },
+      items: 1,
+    },
+    desktop: {
+      breakpoint: { max: 1600, min: 1024 },
+      items: 1,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+
+  return (
+    <div className="py-14">
+      <Carousel
+        ref={ref}
+        responsive={responsive}
+        arrows={false}
+        swipeable={false}
+        draggable={true}
+        showDots={false}
+        infinite={true}
+        autoPlay={true}
+        autoPlaySpeed={3000}
+        keyBoardControl={true}
+        customTransition="all 0.5s"
+        transitionDuration={500}
+        containerClass="carousel-container"
+        removeArrowOnDeviceType={["tablet", "mobile"]}
+        dotListClass="custom-dot-list-style"
+        itemClass="carousel-item-padding-40-px"
+      >
+        {/* Testimonial Item 1 */}
+        <div className="w-full h-[555px] flex flex-row">
+          <div className='w-full h-[555px] relative bg-[#FEBF00] flex-[3]'>
+            <div className='absolute bottom-16 left-0'><LeafIcons /></div>
+            <div className='flex h-full justify-center items-center'>
+              <div className='w-[360px] h-[411px] flex flex-col justify-between relative'>
+                <div className='absolute -top-2 -left-6'>
+                  <img src={clon} alt="" />
+                </div>
+                <div>
+                  <p className='font-Roboto-Regular text-xl text-[#0A1425] leading-[34px]'>You can't go wrong with Chicken Mandi, I had it twice. The chicken was cooked perfectly, juicy & soft (usually mandi chicken is a bit dry). I would defiantly recommend it.</p>
+                </div>
+                <div>
+                  <div className='flex justify-between'>
+                    <div>
+                      <h2 className='font-Bebas-Regular text-lg text-[#0A1425]'>Khalid Al Dawsry</h2>
+                      <p className='font-Roboto-Regular text-sm text-[#333333]'>Jeddah, Saudi</p>
+                    </div>
+                    <div className='w-[39.62px] h-[39.62px]'>
+                      <img src={man} className='w-full h-full rounded-full overflow-hidden object-cover' alt="" />
+                      <hr className='w-[40.45px] border-[3px] border-[#BD1F17] mt-3' />
+                    </div>
+                  </div>
+                  <hr className='w-full border border-[#0A1425] mt-3' />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='w-full h-full  flex-[4]'>
+            <div>
+              <img src={video} className='w-full h-full overflow-hidden object-cover' alt="" />
+            </div>
+          </div>
+        </div>
+
+        {/* Testimonial Item 2 */}
+        <div className="w-full h-[555px] flex flex-row">
+          <div className='w-full h-[555px] relative bg-[#FEBF00] flex-[3]'>
+            <div className='absolute bottom-16 left-0'><LeafIcons /></div>
+            <div className='flex h-full justify-center items-center'>
+              <div className='w-[360px] h-[411px] flex flex-col justify-between relative'>
+                <div className='absolute -top-2 -left-6'>
+                  <img src={clon} alt="" />
+                </div>
+                <div>
+                  <p className='font-Roboto-Regular text-xl text-[#0A1425] leading-[34px]'>You can't go wrong with Chicken Mandi, I had it twice. The chicken was cooked perfectly, juicy & soft (usually mandi chicken is a bit dry). I would defiantly recommend it.</p>
+                </div>
+                <div>
+                  <div className='flex justify-between'>
+                    <div>
+                      <h2 className='font-Bebas-Regular text-lg text-[#0A1425]'>Khalid Al Dawsry</h2>
+                      <p className='font-Roboto-Regular text-sm text-[#333333]'>Jeddah, Saudi</p>
+                    </div>
+                    <div className='w-[39.62px] h-[39.62px]'>
+                      <img src={man} className='w-full h-full rounded-full overflow-hidden object-cover' alt="" />
+                      <hr className='w-[40.45px] border-[3px] border-[#BD1F17] mt-3' />
+                    </div>
+                  </div>
+                  <hr className='w-full border border-[#0A1425] mt-3' />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='w-full h-full  flex-[4]'>
+            <div>
+              <img src={video} className='w-full h-full overflow-hidden object-cover' alt="" />
+            </div>
+          </div>
+        </div>
+
+        {/* Testimonial Item 3 */}
+        <div className="w-full h-[555px] flex flex-row">
+          <div className='w-full h-[555px] relative bg-[#FEBF00] flex-[3]'>
+            <div className='absolute bottom-16 left-0'><LeafIcons /></div>
+            <div className='flex h-full justify-center items-center'>
+              <div className='w-[360px] h-[411px] flex flex-col justify-between relative'>
+                <div className='absolute -top-2 -left-6'>
+                  <img src={clon} alt="" />
+                </div>
+                <div>
+                  <p className='font-Roboto-Regular text-xl text-[#0A1425] leading-[34px]'>You can't go wrong with Chicken Mandi, I had it twice. The chicken was cooked perfectly, juicy & soft (usually mandi chicken is a bit dry). I would defiantly recommend it.</p>
+                </div>
+                <div>
+                  <div className='flex justify-between'>
+                    <div>
+                      <h2 className='font-Bebas-Regular text-lg text-[#0A1425]'>Khalid Al Dawsry</h2>
+                      <p className='font-Roboto-Regular text-sm text-[#333333]'>Jeddah, Saudi</p>
+                    </div>
+                    <div className='w-[39.62px] h-[39.62px]'>
+                      <img src={man} className='w-full h-full rounded-full overflow-hidden object-cover' alt="" />
+                      <hr className='w-[40.45px] border-[3px] border-[#BD1F17] mt-3' />
+                    </div>
+                  </div>
+                  <hr className='w-full border border-[#0A1425] mt-3' />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='w-full h-full  flex-[4]'>
+            <div>
+              <img src={video} className='w-full h-full overflow-hidden object-cover' alt="" />
+            </div>
+          </div>
+        </div>
+
+      </Carousel>
+    </div>
+  );
+});
+
+export default TestimonialSlider;
+
--- src/section/Testimonials.jsx
+++ src/section/Testimonials.jsx
@@ -1,34 +1,46 @@
-import React from 'react'
-import tomato from '/public/img/tomato.png'
-import luiba from '/public/img/luiba.png'
-import { LeftArrow, RightArrow } from '../svg/Arrow'
+import React, { useRef } from 'react';
+import tomato from '/public/img/tomato.png';
+import luiba from '/public/img/luiba.png';
+import { LeftArrow, RightArrow } from '../svg/Arrow';
+import TestimonialSlider from '../component/Testimonials';
 
 const Testimonials = () => {
+    const carouselRef = useRef(null);
+    const handleNext = () => {
+        carouselRef.current.next();
+    };
+
+    const handlePrevious = () => {
+        carouselRef.current.previous();
+    };
     return (
-        <>
-            <section>
-                <div className='w-full h-screen bg-[#ffffff] relative'>
-                    <div className='absolute top-44 left-0'><img src={tomato} alt="" /></div>
-                    <div className='absolute bottom-0 right-0'><img src={luiba} alt="" /></div>
-                    <div className='max-w-container mx-auto py-32'>
-                        <div className='flex justify-between items-center' >
-                            <div>
-                                <div className='flex items-center gap-2'>
-                                    <div className='w-2.5 h-2.5 bg-[#BD1F17]'></div>
-                                    <h3 className='font-Roboto-Bold text-[#BD1F17] text-xl'>Crispy, Every Bite Taste</h3>
-                                </div>
-                                <h1 className='font-Bebas-Regular text-[#181818] text-6xl mt-5'>What Some of my Customers Say</h1>
+        <section>
+            <div className="w-full h-screen bg-[#ffffff] relative">
+                <div className="absolute top-44 left-0"><img src={tomato} alt="tomato" /></div>
+                <div className="absolute -bottom-28 right-0"><img src={luiba} alt="luiba" /></div>
+                <div className="max-w-container mx-auto py-32">
+                    <div className="flex justify-between items-center">
+                        <div>
+                            <div className="flex items-center gap-2">
+                                <div className="w-2.5 h-2.5 bg-[#BD1F17]"></div>
+                                <h3 className="font-Roboto-Bold text-[#BD1F17] text-xl">Crispy, Every Bite Taste</h3>
                             </div>
-                            <div className=' flex gap-5 '>
-                                <button className='flex items-center justify-center py-2 px-4 w-[60px] h-[60px] bg-[#FFFFFF] rounded-full shadow-xl shadow-[#0000001A] hover:text-[#BD1F17]'  ><LeftArrow /></button>
-                                <button className='flex items-center justify-center py-2 px-4 w-[60px] h-[60px] bg-[#FFFFFF] rounded-full shadow-xl shadow-[#0000001A] hover:text-[#BD1F17]'  ><RightArrow /></button>
-                            </div>
+                            <h1 className="font-Bebas-Regular text-[#181818] text-6xl mt-5">What Some of my Customers Say</h1>
+                        </div>
+                        <div className="flex gap-5">
+                            <button className="flex items-center justify-center py-2 px-4 w-[60px] h-[60px] bg-[#FFFFFF] rounded-full shadow-xl shadow-[#0000001A] hover:text-[#BD1F17]" onClick={handleNext}>
+                                <LeftArrow />
+                            </button>
+                            <button className="flex items-center justify-center py-2 px-4 w-[60px] h-[60px] bg-[#FFFFFF] rounded-full shadow-xl shadow-[#0000001A] hover:text-[#BD1F17]" onClick={handlePrevious}>
+                                <RightArrow />
+                            </button>
                         </div>
                     </div>
+                    <TestimonialSlider ref={carouselRef} />
                 </div>
-            </section>
-        </>
-    )
-}
+            </div>
+        </section>
+    );
+};
 
-export default Testimonials
+export default Testimonials;
