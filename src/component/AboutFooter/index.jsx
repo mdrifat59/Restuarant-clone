ce4291c0eddefbd1dbd36f8c3e06b7aa2b9ac144
mdrifat59 <mdrifatulislam59@gmail.com>
2024-11-17 12:36:02 +0600

    About section is responsive done

--- src/component/AboutFooter/index.jsx
+++ src/component/AboutFooter/index.jsx
@@ -6,14 +6,14 @@ import { BagIcons } from '../../svg/Bag'
 const AboutFooter = () => {
     return (
         <>
-            <div className='flex flex-col-3 justify-between py-16 '>
+            <div className="flex flex-col gap-10 lg:grid lg:grid-cols-3 md:justify-between pt-[50px] pb-[32px] 2xl:pt-[100px] ">
                 <div className='flex gap-5 items-center'>
                     <div className='w-20 h-20 rounded-full bg-[#FFFFFF] shadow-xl  shadow-[#0000001A]  flex justify-center items-center'>
                         <DeliveryIcons />
                     </div>
                     <div>
-                        <h3 className='font-Bebas-Regular font-bold  text-[#0A1425] text-3xl'>fast delivery</h3>
-                        <p className='font-Roboto-Regular text-[#0A1425] text-xl'>Within 30 minutes</p>
+                        <h3 className='font-Bebas-Regular text-[#0A1425] text-3xl'>fast delivery</h3>
+                        <p className='font-Roboto-Regular text-[#0A1425] xl:text-xl'>Within 30 minutes</p>
                     </div>
                 </div>
                 <div className='flex gap-5 items-center'>
@@ -21,8 +21,8 @@ const AboutFooter = () => {
                         <DiningIcons />
                     </div>
                     <div>
-                        <h3 className='font-Bebas-Regular font-bold  text-[#0A1425] text-3xl'>absolute dining</h3>
-                        <p className='font-Roboto-Regular text-[#0A1425] text-xl'>Best buffet restaurant</p>
+                        <h3 className='font-Bebas-Regular text-[#0A1425] text-3xl'>absolute dining</h3>
+                        <p className='font-Roboto-Regular text-[#0A1425] xl:text-xl'>Best buffet restaurant</p>
                     </div>
                 </div>
                 <div className='flex gap-5 items-center'>
@@ -30,8 +30,8 @@ const AboutFooter = () => {
                         <BagIcons />
                     </div>
                     <div>
-                        <h3 className='font-Bebas-Regular font-bold  text-[#0A1425] text-3xl'>pickup delivery</h3>
-                        <p className='font-Roboto-Regular text-[#0A1425] text-xl'>Grab your food order</p>
+                        <h3 className='font-Bebas-Regular text-[#0A1425] text-3xl'>pickup delivery</h3>
+                        <p className='font-Roboto-Regular text-[#0A1425] xl:text-xl'>Grab your food order</p>
                     </div>
                 </div>
             </div>
--- src/section/About.jsx
+++ src/section/About.jsx
@@ -13,14 +13,15 @@ const About = () => {
     return (
         <>
             <section>
-                <div className='w-full h-screen py-32 relative'>
-                    <div className='absolute bottom-20 right-0'><img src={aboutsite} alt="" /></div>
-                    <div className='max-w-container mx-auto ' >
-                        <div className='grid grid-cols-2'>
+                <div className='w-full py-[32px] md:py-[70px] lg:py-[120px] relative'>
+                    <div className='absolute bottom-20 right-0 hidden 2xl:block'><img src={aboutsite} alt="" /></div>
+
+                    <div className='w-full px-4 sm:px-6 md:px-8 lg:max-w-container lg:mx-auto ' >
+                        <div className='grid lg:grid-cols-2 lg:gap-5 2xl:gap-10 3xl:gap-20'>
                             <div className='relative'>
-                                <img src={Aboutimg} alt="" />
-                                <div className='w-[241.17px] h-[136.75px] bg-[#FFFFFF] rounded-lg flex gap-5 p-2 items-center absolute top-5 left-5'>
-                                    <div>
+                                <img src={Aboutimg} className='sm:w-full lg:h-full' alt="" />
+                                <div className='w-[128px] h-[73px] sm:w-[150px] sm:h-[90px] md:w-[170px] md:h-[120px] lg:w-[241.17px] lg:h-[136.75px]  bg-[#FFFFFF] rounded-lg lg:flex gap-5 p-2 items-center absolute top-5 left-5'>
+                                    <div className='xs:w-[50px] xs:h-[50px] bg-green-400 hidden'>
                                         <CircularSlider
                                             labelColor="#181818"
                                             knobColor="#FEBF00"
@@ -35,7 +36,7 @@ const About = () => {
                                             valueFontSize='2'
                                             labelFontSize='50'
                                             label=' '
-                                            verticalOffset='center'
+                                            verticalOffset='center' 
                                         />
                                     </div>
                                     <div>
@@ -44,18 +45,18 @@ const About = () => {
                                     </div>
                                 </div>
                             </div>
-                            <div className=''>
+                            <div className='pt-[32px] sm:pt-[50px] lg:pt-[0px]'>
                                 <div className='w-full '>
                                     <button className={`font-Roboto-Medium mr-5 text-[#333333]  ${isvisiable === "about" && 'text-[#FFFFFF] bg-[#B52B1D]'} py-1.5 px-4`} onClick={handleAbout}>About</button>
                                     <button className={`font-Roboto-Medium mr-5 text-[#333333]  ${isvisiable === "experience" && 'text-[#FFFFFF] bg-[#B52B1D]'} py-1.5 px-4`} onClick={handleExperience}>Experience</button>
                                     <button className={`font-Roboto-Medium mr-5 text-[#333333]  ${isvisiable === "contact" && 'text-[#FFFFFF] bg-[#B52B1D]'} py-1.5 px-4`} onClick={handleContact}>Contact</button>
                                 </div>
-                                <hr className='border-0 h-px bg-[#B52B1D]' />
+                                <hr className='border-0 h-[1px] md:h-[2px] bg-[#B52B1D]' />
                                 {
                                     isvisiable === "about" ?
                                         <div className='w-full h-full' >
-                                            <h2 className='font-Bebas-Regular font-bold text-[62px] leading-[62px] py-5'>Exceptional culinary experience and delicious food</h2>
-                                            <p className='font-Roboto-Regular text-[#333333]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ornare non sed est cursus. Vel hac convallis ipsum, facilisi odio pellentesque bibendum viverra tempus. Lorem ipsum dolor sit amet consectetur adipiscing elit do eiusmod tempor incididunt ut labore et dolore magna minim veniam nostrud exercitation.</p>
+                                            <h2 className='font-Bebas-Regular w-[330px] md:w-[520px]  leading-[48px] md:leading-[70px] lg:w-[510px] xl:w-[520px] lg:text-[55px] xl:text-[62px] text-[40px] md:text-[62px] lg:leading-[62px] py-5'>Exceptional culinary experience and delicious food</h2>
+                                            <p className='font-Roboto-Regular w-[330px] md:w-full text-[#333333]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ornare non sed est cursus. Vel hac convallis ipsum, facilisi odio pellentesque bibendum viverra tempus. Lorem ipsum dolor sit amet consectetur adipiscing elit do eiusmod tempor incididunt ut labore et dolore magna minim veniam nostrud exercitation.</p>
                                             <div className='flex gap-8 items-center mt-10'>
                                                 <button className='font-Roboto-Bold text-[#0A1425] uppercase bg-[#FEBF00] px-6 py-4'>About More</button>
                                                 <div className='flex gap-2 items-center'>
@@ -67,7 +68,7 @@ const About = () => {
                                         :
                                         isvisiable === "experience" ?
                                             <div className='w-full h-full' >
-                                                <h2 className='font-Bebas-Regular font-bold text-[62px] leading-[62px] py-5'>Experience section</h2>
+                                                <h2 className='font-Bebas-Regular w-[330px] md:w-[520px]  leading-[48px] md:leading-[70px] lg:w-[510px] xl:w-[520px] lg:text-[55px] xl:text-[62px] text-[40px] md:text-[62px] lg:leading-[62px] py-5'>Experience section</h2>
                                                 <p className='font-Roboto-Regular text-[#333333]'> This is me, consectetur adipiscing elit. Ornare non sed est cursus. Vel hac convallis ipsum, facilisi odio pellentesque bibendum viverra tempus. Lorem ipsum dolor sit amet consectetur adipiscing elit do eiusmod tempor incididunt ut labore et dolore magna minim veniam nostrud exercitation.</p>
                                                 <div className='flex gap-8 items-center mt-10'>
                                                     <button className='font-Roboto-Bold text-[#0A1425] uppercase bg-[#FEBF00] px-6 py-4'>About More</button>
@@ -79,7 +80,7 @@ const About = () => {
                                             </div>
                                             :
                                             <div className='w-full h-full' >
-                                                <h2 className='font-Bebas-Regular font-bold text-[62px] leading-[62px] py-5'>Contact with us</h2>
+                                                <h2 className='font-Bebas-Regular w-[330px] md:w-[520px]  leading-[48px] md:leading-[70px] lg:w-[510px] xl:w-[520px] lg:text-[55px] xl:text-[62px] text-[40px] md:text-[62px] lg:leading-[62px] py-5'>Contact with us</h2>
                                                 <p className='font-Roboto-Regular text-[#333333]'> That is me, consectetur adipiscing elit. Ornare non sed est cursus. Vel hac convallis ipsum, facilisi odio pellentesque bibendum viverra tempus. Lorem ipsum dolor sit amet consectetur adipiscing elit do eiusmod tempor incididunt ut labore et dolore magna minim veniam nostrud exercitation.</p>
                                                 <div className='flex gap-8 items-center mt-10'>
                                                     <button className='font-Roboto-Bold text-[#0A1425] uppercase bg-[#FEBF00] px-6 py-4'>About More</button>
