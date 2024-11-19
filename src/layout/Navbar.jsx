0287cd935d5d831f4112c47fe9a4550ca1f85c4d
mdrifat59 <mdrifatulislam59@gmail.com>
2024-11-19 18:42:21 +0600

    responsive menu botton done

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -4,6 +4,7 @@ import { MenuIcons } from '../svg/Menu'
 
 const Navbar = () => {
   const [scrolled, setScrolled] = useState(false)
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
 
   useEffect(() => {
     const handleScroll = () => {
@@ -18,7 +19,7 @@ const Navbar = () => {
     return () => window.removeEventListener("scroll", handleScroll)
   }, [])
   const toggleMenu = () => {
-
+    setIsMenuOpen(!isMenuOpen)
   }
   return (
     <>
@@ -43,13 +44,34 @@ const Navbar = () => {
 
 
           </div>
-
+          {/* short menu botton */}
           <div
             className="w-[40px]   h-[30px] md:w-[40px] md:h-[40px]  xl:hidden  flex justify-center items-center cursor-pointer"
             onClick={toggleMenu} // Toggle the menu on click
           >
             <MenuIcons className="lg:h-[30px] lg:w-[30px]   text-white" />
           </div>
+          {/* short menu */}
+          {
+            isMenuOpen &&
+            <div className='xl:hidden absolute top-full left-0 w-full bg-[#1e1e1e] p-5 shadow-lg z-[999] transition-all duration-300 content-[""]'>
+              <div className='flex flex-col items-start md:items-center gap-5 text-white'>
+
+                <ul className='text-[#FFFFFF] grid gap-2 '>
+                  <li className='font-Raleway-Medium text-[15px]  relative after:absolute after:content-[""] after:w-0 after:h-[1px] after:bottom-[-2px] after:left-0 after:bg-white after:transition-all after:duration-200 hover:after:w-full cursor-pointer' onClick={toggleMenu}>Home</li>
+                  <li className='font-Raleway-Medium text-[15px] relative after:absolute after:content-[""] after:w-0 after:h-[1px] after:bottom-[-2px] after:left-0 after:bg-white after:transition-all after:duration-200 hover:after:w-full cursor-pointer' onClick={toggleMenu}>About</li>
+                  <li className='font-Raleway-Medium text-[15px] relative after:absolute after:content-[""] after:w-0 after:h-[1px] after:bottom-[-2px] after:left-0 after:bg-white after:transition-all after:duration-200 hover:after:w-full cursor-pointer' onClick={toggleMenu}>Portfolio</li>
+                  <li className='font-Raleway-Medium text-[15px] relative after:absolute after:content-[""] after:w-0 after:h-[1px] after:bottom-[-2px] after:left-0 after:bg-white after:transition-all after:duration-200 hover:after:w-full cursor-pointer' onClick={toggleMenu}>Clients</li>
+                  <li className='font-Raleway-Medium text-[15px] relative after:absolute after:content-[""] after:w-0 after:h-[1px] after:bottom-[-2px] after:left-0 after:bg-white after:transition-all after:duration-200 hover:after:w-full cursor-pointer' onClick={toggleMenu}>Blog</li>
+                  <li className='font-Raleway-Medium text-[15px] relative after:absolute after:content-[""] after:w-0 after:h-[1px] after:bottom-[-2px] after:left-0 after:bg-white after:transition-all after:duration-200 hover:after:w-full cursor-pointer' onClick={toggleMenu}>Contact</li>
+                </ul>
+                <div className='md:hidden '>
+                  <button className='text-[#0A1425] font-Roboto-Bold py-[10px] px-6 bg-[#FEBF00] uppercase text-base'>Book a Table</button>
+                </div>
+              </div>
+            </div>
+
+          }
 
           <div className='lg:flex items-center justify-end md:block hidden'>
             <button className='text-[#0A1425] font-Roboto-Bold py-[10px] px-6 bg-[#FEBF00] uppercase text-base'>Book a Table</button>
